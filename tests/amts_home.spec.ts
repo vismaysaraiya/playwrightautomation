d24a39b20d372f5463c135a9651539919826b47e
vismaysaraiya <vismaysaraiya@gmail.com>
2026-06-27 20:42:52 +0530

    new commit

---
+++ tests/amts_home.spec.ts
@@ -0,0 +1,5 @@
+import { NoDriverProxyCommandError } from "appium/build/lib/appium"
+import { findPackageJSON } from "node:module"
+
+test tues 
+NoDriverProxyCommandError findPackageJSON debuggerd f
